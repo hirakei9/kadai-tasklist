ecc4811e3e91501828c7213500348f278127ede0
keita.hirasawa <ke-ta@hotmail.co.jp>
2019-08-03 14:11:01 +0000

    kadai-1

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,13 @@
 Rails.application.routes.draw do
   root to: 'tasks#index'
-  resources :tasks
   
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   
   
-  # edit: 更新用のフォームページ
-  get 'tasks/:id/edit', to: 'tasks#edit'
-  
   get "signup", to: "users#new"
-  resources :users, only: [:index, :show, :new, :create]
+  resources :users, only: [:show, :new, :create]
   
   resources :tasks, only: [:create, :destroy, :edit, :show, :new , :index]
 end
